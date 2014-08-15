5d5dc5fb90465f8a34f1b6fe3ea94f86f0686754
Graham Rogers <gmrogers04@gmail.com>
2014-08-15 18:15:18 -0500

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name uiRouterApp
+ * @description
+ * # uiRouterApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('uiRouterApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch',
+        'ui.router'
+  ]);
